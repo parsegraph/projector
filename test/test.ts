40a6d098fa30e0d669e3ccaa78d3d6369fb519f3
Aaron Faanes <dafrito@gmail.com>
2021-12-17 00:26:21 -0600

    minor lint

--- test/test.ts
+++ test/test.ts
@@ -1,8 +1,8 @@
-import {assert} from "chai";
+import { assert } from "chai";
 import todo from "../dist/parsegraph-projector";
 
 describe("Package", function () {
-  it("works", ()=>{
+  it("works", () => {
     assert.equal(todo(), 42);
   });
 });
