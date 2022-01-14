a0112984f3a80bdbca92eb76c2f5a9bd32223fed
Aaron Faanes <dafrito@gmail.com>
2022-01-13 23:10:38 -0600

    Properly implement index

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,16 @@
-// TODO Implement this.
-export default function todo() {
-  return 42;
-}
+import BasicProjector, {MAX_TEXTURE_SIZE, BACKGROUND_COLOR} from "./BasicProjector";
+import Projected from "./Projected";
+import Projection from "./Projection";
+import Projector, {createDOMContainer} from "./Projector";
+import SharedProjector from "./SharedProjector";
+
+export {
+  BasicProjector,
+  MAX_TEXTURE_SIZE,
+  BACKGROUND_COLOR,
+  Projected,
+  Projection,
+  Projector,
+  createDOMContainer,
+  SharedProjector
+};
