c70dd426cbb2ff61af2717910b9efe1b21b3ba8c
Aaron Faanes <dafrito@gmail.com>
2022-02-26 17:21:09 -0600

    Upgrade microproject files

--- install-deps.sh
+++ install-deps.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-yarn add --immutable --immutable-cache --check-cache
-yarn add --no-lockfile `node -e "Object.keys(JSON.parse(require('fs').readFileSync('package.json')).peerDependencies || {}).forEach(dep=>console.log(dep))"`
+yarn install --immutable --immutable-cache --check-cache
+yarn add -P --no-lockfile `node -e "Object.keys(JSON.parse(require('fs').readFileSync('package.json')).peerDependencies || {}).forEach(dep=>console.log(dep))"`
