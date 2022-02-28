0cacc7a8bdd807ce64d61e3f7775d80a57b8f5c6
Aaron Faanes <dafrito@gmail.com>
2022-02-27 19:49:55 -0600

    Remove parsegraph-color

--- test/test.ts
+++ test/test.ts
@@ -1,11 +1,10 @@
 import { assert } from "chai";
 import { BasicProjector } from "../src/index";
-import Color from "parsegraph-color";
 
 describe("Package", function () {
   global.ResizeObserver = require("resize-observer-polyfill");
   window.ResizeObserver = require("resize-observer-polyfill");
   it("works", () => {
-    assert.ok(new BasicProjector(new Color(0, 0, 0, 1)));
+    assert.ok(new BasicProjector());
   });
 });
